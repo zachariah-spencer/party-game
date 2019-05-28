1f51f5331820f8b12d8a29ef642b35fe80924e1e
TheMikirog <slimeyninjav@gmail.com>
2019-05-28 21:05:43 +0200

    added animations

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+# TODO: implement mirroring on turning (without glitching out the arms)
+
 var gravity
 var velocity
 export var max_speed : int
@@ -11,17 +13,24 @@ export var jump_height : int
 func _ready():
     velocity = Vector2(0,0)
     gravity = Globals.GRAVITY
+    $AnimationTree.set_active(true)
     
     #scaling variables
     jump_height *= 100
     max_speed *= 10
+    
 
 func _physics_process(delta):
     
     #gravity
     if is_on_floor():
+        if velocity.y != 0: $AnimationTree['parameters/playback'].travel('Grounded') #change animation on landing
         velocity.y = 0
     else:
+        if Input.is_action_pressed('ui_right') or Input.is_action_pressed('ui_left'):
+            $AnimationTree['parameters/Grounded/playback'].travel('Run')
+        else:
+            $AnimationTree['parameters/Grounded/playback'].travel('Idle')
         velocity.y += gravity
     
     #left and right movement
@@ -41,6 +50,10 @@ func _physics_process(delta):
     
     #jump movement
     if Input.is_action_pressed('ui_accept') && is_on_floor():
+        $AnimationTree['parameters/playback'].travel('Airborne') #set animation on jump
         velocity.y = -jump_height
+    
+    #set jumping/falling animation depending on the vertical velocity
+    $AnimationTree['parameters/Airborne/blend_position'] = velocity.y / 300
     
     move_and_slide(velocity, Globals.UP)
