11f179e9eab571a4bb6ffa0891566ed397119417
zachariah-spencer <spencerzachariah@gmail.com>
2019-06-06 18:23:48 -0500

    update

--- scripts/hud_scripts/RaceTowerHUD.gd
+++ scripts/hud_scripts/RaceTowerHUD.gd
@@ -5,5 +5,10 @@ extends HUD
 func _ready():
 	pass
 
+func _physics_process(delta):
+	pass
+	#set so that the hud centers on the Y position of the camera's center
+
+
 func _on_Timer_timeout():
 	_every_second()
