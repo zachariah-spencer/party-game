c10afd4f191534f04d6c98d354d9e43e05e2b4f3
cslaf <cade.schlaefli@gmail.com>
2019-06-11 17:11:57 -0500

    implemented new player input method and directional attacks

--- scripts/player_scripts/PlayerTwo.gd
+++ scripts/player_scripts/PlayerTwo.gd
@@ -12,6 +12,7 @@ func _ready():
 func register_player_inputs():
 	child.move_left = 'player_two_move_left'
 	child.move_right = 'player_two_move_right'
+	child.move_up = 'player_two_move_up'
 	child.move_jump = 'player_two_move_jump'
 	child.move_down = 'player_two_move_down'
 	child.attack_input = 'player_two_attack'
