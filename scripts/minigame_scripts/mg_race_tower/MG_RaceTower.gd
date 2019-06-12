98eae0e8ac14a7d926ec5667932348e45dfd01a2
cslaf <cade.schlaefli@gmail.com>
2019-06-12 10:33:51 -0500

    minor changes to lava code

--- scripts/minigame_scripts/mg_race_tower/MG_RaceTower.gd
+++ scripts/minigame_scripts/mg_race_tower/MG_RaceTower.gd
@@ -2,13 +2,12 @@ extends Minigame
 
 #When a new map chunk scene to be randomly generated is created, adding it to this list
 #should insert it into the procedural rotation
-const CHUNKS : Dictionary = {
-	'chunk_one' : preload('res://scenes/minigames/mg_race_tower/TowerChunkOne.tscn'),
-	'chunk_two' : preload('res://scenes/minigames/mg_race_tower/TowerChunkTwo.tscn'),
-	'chunk_three' : preload('res://scenes/minigames/mg_race_tower/TowerChunkThree.tscn'),
-}
+const CHUNKS  = [
+	preload('res://scenes/minigames/mg_race_tower/TowerChunkOne.tscn'),
+	preload('res://scenes/minigames/mg_race_tower/TowerChunkTwo.tscn'),
+	preload('res://scenes/minigames/mg_race_tower/TowerChunkThree.tscn')
+]
 
-var loaded_chunks : Array = []
 signal updated_chunks
 
 func _ready():
@@ -21,7 +20,7 @@ func _ready():
 
 func _physics_process(delta):
 	_run_minigame_loop()
-	
+
 
 func _run_minigame_loop():
 	if game_active:
@@ -40,8 +39,8 @@ func _game_won(no_winner = false):
 			$CanvasLayer/HUD/TimeLeft/Instructions.text = $CanvasLayer/HUD/TimeLeft/Instructions.text +  player.display_name + '\n'
 	elif no_winner:
 		$CanvasLayer/HUD/TimeLeft/Instructions.text = 'Nobody Won!'
-	
-	
+
+
 
 func _check_game_win_conditions():
 	if game_time == 0 || Players._get_alive_players().size() == 0:
@@ -53,26 +52,34 @@ func _check_game_win_conditions():
 func _on_Lava_body_entered(player):
 	player.hit_points = 0
 
-func update_chunk(chunk_pos : Vector2, old_chunk : Node2D):
+func update_chunk(old_chunk : Node2D):
 	#This instancing is causing extreme lag
-	#Unsure if this is a code problem or merely 
-	#a node-count issue as we are using individual 
+	#Unsure if this is a code problem or merely
+	#a node-count issue as we are using individual
 	#nodes for each 64x64 block of terrain
-	
+
 	#let all active chunks know that more are being loaded
 	emit_signal('updated_chunks')
-	var new_chunk
-	
+	CHUNKS.shuffle()
+	var new_chunk = CHUNKS[0]
+	#add a field unique to each chunk for this work.
+	#We could have entrances and exits to each chunk and match them here
+	#To address the bottom chunk, just instance it in the scene
+	#and don't add it to the array.
+	while new_chunk == old_chunk :
+		CHUNKS.shuffle()
+		new_chunk = CHUNKS[0]
+
 	#do-while to randomize the pieces of map that are loading and ensure you never get the same piece twice
 	#also an edge case that stops the 'bottom tower' map chunk from being randomly selected
-	new_chunk = CHUNKS[CHUNKS.keys()[int(rand_range(1,CHUNKS.size()))]]
-	while new_chunk == old_chunk:
-		new_chunk = CHUNKS[CHUNKS.keys()[int(rand_range(1,CHUNKS.size()))]]
-	
+#	new_chunk = CHUNKS[CHUNKS.keys()[int(rand_range(1,CHUNKS.size()))]]
+#	while new_chunk == old_chunk:
+#		new_chunk = CHUNKS[CHUNKS.keys()[int(rand_range(1,CHUNKS.size()))]]
+
 	#instance and set new chunk above players on the top of the tower
 	new_chunk = new_chunk.instance()
-	new_chunk.position = chunk_pos
-	new_chunk.position.y -= 4096
-	
+	new_chunk.position = old_chunk.position
+	new_chunk.position.y -= old_chunk.size
+
 	#add to tree
 	call_deferred('add_child', new_chunk)
--- scripts/minigame_scripts/mg_race_tower/TowerChunk.gd
+++ scripts/minigame_scripts/mg_race_tower/TowerChunk.gd
@@ -3,18 +3,22 @@ extends Node2D
 onready var parent : Minigame = get_parent()
 var entered := false
 var memory_counter := 0
+var size := 4096
 
 func _ready():
-	yield(get_tree().create_timer(1),'timeout')
+	#why?
+#	yield(get_tree().create_timer(1),'timeout')
 	$Area.connect('body_entered', self, 'chunk_entered')
 	parent.connect('updated_chunks', self ,'on_chunk_updates')
 
 func chunk_entered(body):
-	if entered == false:
-		entered = true
-		get_parent().update_chunk(position, self)
+	if body.is_in_group("player") :
+		if entered == false:
+			entered = true
+			get_parent().update_chunk(self)
 
 func on_chunk_updates():
-	memory_counter += 1
-	if memory_counter > 1:
-		queue_free()
+	pass
+#	memory_counter += 1
+#	if memory_counter > 1:
+#		queue_free()
