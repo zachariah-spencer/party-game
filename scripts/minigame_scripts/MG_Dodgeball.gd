8c999d960a2bbd44e8b9b6179c7c1ad67d9bc416
zachariah-spencer <spencerzachariah@gmail.com>
2019-06-15 16:53:57 -0500

    refactor _check_game_win_conditions and _game_won into Minigame.gd and establish a win_conditions enum/win_condition property for each minigame that determines what version of each method will be called.

--- scripts/minigame_scripts/MG_Dodgeball.gd
+++ scripts/minigame_scripts/MG_Dodgeball.gd
@@ -15,22 +15,4 @@ func _run_minigame_loop():
 	if game_active:
 		_check_game_win_conditions()
 
-func _game_won(no_winner = false):
-	game_active = false
-	game_over = true
-	if !no_winner:
-		Players._get_alive_players()[0].score += 1
-		$CanvasLayer/HUD._update_scores()
-		$CanvasLayer/HUD/TimeLeft/Instructions.text = Players._get_alive_players()[0].display_name + ' Won!'
-	else:
-		$CanvasLayer/HUD/TimeLeft/Instructions.text = 'Nobody Won!'
-
-func _check_game_win_conditions():
-	if Players._get_alive_players().size() == 1:
-		_game_won()
-	elif Players._get_alive_players().size() >= 1 && game_time == 0:
-		_game_won()
-	elif Players._get_alive_players().size() == 0 || game_time == 0:
-		_game_won(true)
-
 
