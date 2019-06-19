2ea6208001790fefbea8aaf8ad714631f762d48a
zachariah-spencer <spencerzachariah@gmail.com>
2019-06-18 22:09:12 -0500

    fix lag in punchball motion + make punchball knockback relative to players position

--- scripts/items/Punchball.gd
+++ scripts/items/Punchball.gd
@@ -1,20 +1,13 @@
 extends Item
 onready var impulse_timer := $ImpulseTimer
-onready var punch_area := $PunchedArea
 onready var hit_cooldown := $HitCooldownTimer
-onready var nearby_player_area := $NearbyPlayerArea
-onready var raycasts := $Raycasts.get_children()
-onready var bottom_raycast := $Raycasts/BottomCast
-onready var top_raycast := $Raycasts/TopCast
-onready var left_raycast := $Raycasts/LeftCast
-onready var right_raycast := $Raycasts/RightCast
+onready var punch_area := $PunchedArea
 onready var runaway_area := $RunawayArea
+export var random_speed : float
+export var runaway_speed := Globals.CELL_SIZE * 0.5
 var player_near_ball : Player
-var random_impulse_speed : float
-var runaway_speed := Globals.CELL_SIZE * 0.5
-var max_runaway_speed := Globals.CELL_SIZE * 5
-var runaway_vector : Vector2 = Vector2.ZERO
 var can_be_hit := true
+
 signal player_got_a_punch
 
 func _ready():
@@ -24,33 +17,33 @@ func _ready():
 
 func _physics_process(delta):
 	if player_near_ball:
-		_handle_runaway()
+		_handle_runaway(delta)
 
-func _handle_runaway():
+func _handle_runaway(delta : float):
+	var runaway_velocity : Vector2 = Vector2.ZERO
+	
 	if player_near_ball:
-		var runaway_velocity : Vector2 = Vector2.ZERO
-		print(player_near_ball.global_position)
-		print(global_position)
-		runaway_velocity = ( (player_near_ball.global_position - global_position).normalized() ) * Globals.CELL_SIZE / 4
-		print(runaway_velocity)
+		runaway_velocity = ( (player_near_ball.global_position - global_position).normalized() ) * Globals.CELL_SIZE / 3
 		apply_central_impulse(-runaway_velocity)
 
-func _on_ImpulseTimer_timeout():
+func _handle_random_motion():
+	var impulse_vector : Vector2
+	var do_flip_x := randi() % 2
+	var do_flip_y := randi() % 2
 	
+	impulse_vector.x = rand_range(Globals.CELL_SIZE * 25, Globals.CELL_SIZE * 50)
+	impulse_vector.y = rand_range(Globals.CELL_SIZE * 25, Globals.CELL_SIZE * 50)
+	
+	if do_flip_x:
+		impulse_vector.x *= -1
+	if do_flip_y:
+		impulse_vector.y *= -1
+	
+	apply_central_impulse(impulse_vector)
+
+func _on_ImpulseTimer_timeout():
 	if !player_near_ball:
-		var impulse_vector : Vector2
-		var do_flip_x := randi() % 2
-		var do_flip_y := randi() % 2
-		
-		impulse_vector.x = rand_range(Globals.CELL_SIZE * 25, Globals.CELL_SIZE * 50)
-		impulse_vector.y = rand_range(Globals.CELL_SIZE * 25, Globals.CELL_SIZE * 50)
-		
-		if do_flip_x:
-			impulse_vector.x *= -1
-		if do_flip_y:
-			impulse_vector.y *= -1
-		
-		apply_central_impulse(impulse_vector)
+		_handle_random_motion()
 
 
 func _on_PunchedArea_body_entered(body):
@@ -81,6 +74,19 @@ func _on_PunchedArea_body_entered(body):
 		if player_fist && !get_parent().game_over:
 			emit_signal('player_got_a_punch', attacking_player, 1)
 
+func hit(by : Node, damage : int, knockback :Vector2):
+	
+	if can_be_hit && by.is_attacking:
+		var knockback_velocity : Vector2 = Vector2.ZERO
+	
+		knockback_velocity = (knockback) * Globals.CELL_SIZE / 2
+		apply_central_impulse(-knockback_velocity)
+		
+		can_be_hit = false
+		hit_cooldown.start()
+		modulate.a = .5
+		if !get_parent().game_over:
+			emit_signal('player_got_a_punch', by, 1)
 
 func _on_HitCooldownTimer_timeout():
 	can_be_hit = true
