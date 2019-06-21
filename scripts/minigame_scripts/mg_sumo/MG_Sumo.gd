b651fbc861f12e7d2658b32874400d8b235fd9da
zachariah-spencer <spencerzachariah@gmail.com>
2019-06-21 16:03:45 -0500

    factor deathboundaries into their own scene so maps can be made where players can fall out of bounds and die

--- scripts/minigame_scripts/mg_sumo/MG_Sumo.gd
+++ scripts/minigame_scripts/mg_sumo/MG_Sumo.gd
@@ -6,9 +6,6 @@ func _ready():
 	game_instructions = "Punch The Other\nPlayers Off!"
 	game_time = 15
 	win_condition = win_conditions.last_alive_allow_no_winners
-#	$Cam.current = true
-
-	$DeathBoundary.connect("body_entered", self, 'on_out_of_bounds')
 
 func _physics_process(delta):
 	_run_minigame_loop()
@@ -17,7 +14,3 @@ func _run_minigame_loop():
 
 	if game_active:
 		_check_game_win_conditions()
-
-func on_out_of_bounds(body):
-	if body.get_parent().is_in_group('players'):
-		body.hit_points = 0
