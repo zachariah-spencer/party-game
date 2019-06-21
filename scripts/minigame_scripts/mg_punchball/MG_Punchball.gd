3a82e1af020ae174262c316568d63244a5a97ee9
zachariah-spencer <spencerzachariah@gmail.com>
2019-06-21 15:57:37 -0500

    add an any-player map for punchball

--- scripts/minigame_scripts/mg_punchball/MG_Punchball.gd
+++ scripts/minigame_scripts/mg_punchball/MG_Punchball.gd
@@ -4,7 +4,8 @@ func _init():
 	maps = [
 	preload('res://scenes/minigames/mg_punchball/maps/4_PunchballMap1.tscn'),
 	preload('res://scenes/minigames/mg_punchball/maps/2_PunchballMap2.tscn'),
-	preload('res://scenes/minigames/mg_punchball/maps/3_PunchballMap3.tscn')
+	preload('res://scenes/minigames/mg_punchball/maps/3_PunchballMap3.tscn'),
+	preload('res://scenes/minigames/mg_punchball/maps/Any_PunchballMap4.tscn')
 	]
 
 func _ready():
