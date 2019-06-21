521f2aa589ef0e9a0f95bbc1ef82815b91546eeb
zachariah-spencer <spencerzachariah@gmail.com>
2019-06-21 11:44:10 -0500

    add 3 player territories map

--- scripts/minigame_scripts/mg_territories/MG_Territories.gd
+++ scripts/minigame_scripts/mg_territories/MG_Territories.gd
@@ -1,5 +1,11 @@
 extends Minigame
 
+func _init():
+	maps = [
+	preload('res://scenes/minigames/mg_territories/maps/4_TerritoriesMap1.tscn'),
+	preload('res://scenes/minigames/mg_territories/maps/3_TerritoriesMap2.tscn')
+	]
+
 func _ready():
 	add_to_group('minigames')
 	Manager.minigame_name = 'territories'
