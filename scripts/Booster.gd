dc6428662335eab449beee0eeb382867c8d2dd03
zachariah-spencer <spencerzachariah@gmail.com>
2019-06-25 18:49:56 -0500

    add booster areas

---
+++ scripts/Booster.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+onready var area = $Area
+
+export var launch_speed = 75
+
+func _ready():
+	area.connect('body_entered', self, 'on_body_entered')
+
+
+func on_body_entered(body):
+	var player = body as Player
+	
+	if player:
+		player.velocity.y = -launch_speed * Globals.CELL_SIZE
