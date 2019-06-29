d062e735657d33b8ff229ee86f7c9841a8b4604b
cslaf <cade.schlaefli@gmail.com>
2019-06-29 18:22:40 -0500

    added material to antigrav

--- scripts/AntigravityArea.gd
+++ scripts/AntigravityArea.gd
@@ -1,6 +1,25 @@
 extends Area2D
 
 func _ready():
+
+	var mat : ParticlesMaterial = $Particles2D.process_material.duplicate()
+	var circle = $Shape.shape as CircleShape2D
+	var rect = $Shape.shape as RectangleShape2D
+	var area = 0
+	if circle :
+		mat.emission_shape = ParticlesMaterial.EMISSION_SHAPE_SPHERE
+		mat.emission_sphere_radius = circle.radius
+		area = circle.radius*circle.radius * PI
+	if rect :
+		mat.emission_shape = ParticlesMaterial.EMISSION_SHAPE_BOX
+		mat.emission_box_extents = Vector3(rect.extents.x, rect.extents.y, 0)
+		area = rect.extents.x * rect.extents.y
+	mat.gravity = Vector3(gravity_vec.x * 100, gravity_vec.y * 100, 0)
+
+	$Particles2D.amount = area * 100
+	$Particles2D.process_material = mat
+
+
 	connect('body_entered',self,'on_body_entered')
 	connect('body_exited',self,'on_body_exited')
 
