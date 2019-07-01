a7d00791c7cbf8d624a60a85ec5df84482d6cbe2
zachariah-spencer <spencerzachariah@gmail.com>
2019-07-01 17:12:38 -0500

    work on pause menu / begin adding options

--- scripts/ui/PauseMenu.gd
+++ scripts/ui/PauseMenu.gd
@@ -1,7 +1,47 @@
 extends Control
 
+var pause_state := false
+
+onready var main := $MainPause
+onready var settings := $Settings
+
+
 func _input(event):
 	if event.is_action_pressed('player_one_pause'):
-		var new_pause_state = not get_tree().paused
-		get_tree().paused = new_pause_state
-		visible = new_pause_state
+		pause_state = not get_tree().paused
+		get_tree().paused = pause_state
+		visible = pause_state
+		
+		if pause_state:
+			settings.visible = false
+			main.visible = true
+
+
+func _on_SettingsButton_pressed():
+	if pause_state:
+		main.visible = false
+		settings.visible = true
+
+func _on_QuitGameButton_pressed():
+	if pause_state:
+		get_tree().quit()
+
+
+
+func _on_MuteSoundsButton_pressed():
+	if pause_state:
+		print('muted sounds')
+
+func _on_MuteMusicButton_pressed():
+	if pause_state:
+		print('muted music')
+
+func _on_BackButton_pressed():
+	if pause_state:
+		settings.visible = false
+		main.visible = true
+
+
+func _on_CheckBox_pressed():
+	var new_state = not OS.window_fullscreen
+	OS.window_fullscreen = new_state
