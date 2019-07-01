d56987779fab28faf9e441d55a50ca2b5b30c0b1
zachariah-spencer <spencerzachariah@gmail.com>
2019-07-01 11:24:31 -0500

    added focus and default_focus groups and configured camera to follow them properly

--- scripts/Camera.gd
+++ scripts/Camera.gd
@@ -1,21 +1,39 @@
 extends Camera2D
 
-export var default_zoom_mod = 1.25
-var alive = 0
-var center = Vector2.ZERO
+export var default_zoom_mod := 1.50
+var num_of_focuses := 0
+var center := Vector2.ZERO
+var focal_points := []
+var no_players := true
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	center = Vector2.ZERO
-	alive = 0
-	for player in Players._get_alive_players():
-		center += player.child.global_position
-		alive += 1
-	if alive > 0 : center = center/alive
-
+	
+	no_players = true if Players._get_alive_players().size() < 1 else false
+	
+	focal_points = get_tree().get_nodes_in_group('focus')
+	
+	num_of_focuses = 0
+	
+	if !no_players:
+		for focal_point in focal_points:
+			
+			if focal_point.is_in_group('default_focus'):
+				focal_points.remove(focal_points.find(focal_point))
+	else:
+		zoom = Vector2.ONE * default_zoom_mod
+	
+	for focal_point in focal_points:
+		center += focal_point.global_position
+		num_of_focuses += 1
+	if num_of_focuses > 0 : center = center / num_of_focuses
+	
 	var dist = 0
-	for player in Players._get_alive_players():
-		dist += center.distance_to(player.child.global_position)
-	if alive > 1 :
+	
+	for focal_point in focal_points:
+		dist += center.distance_to(focal_point.global_position)
+	if num_of_focuses > 1 :
 		if dist < 860:
 			zoom = lerp(zoom, Vector2.ONE * max(.1,log(dist)/log(40)-.3), .08)
 		elif dist >= 860 && dist < 1300:
@@ -28,7 +46,5 @@ func _process(delta):
 			zoom = lerp(zoom, Vector2.ONE * max(.1,log(dist * 35)/log(40)-.3), .08)
 		elif dist >= 2700:
 			zoom = lerp(zoom, Vector2.ONE * max(.1,log(dist * 45)/log(40)-.3), .08)
-	else :
-		zoom = Vector2.ONE * default_zoom_mod
+	
 	global_position = center
-
