497cb40035ea89617d8152c0f635b6f51d82af35
zachariah-spencer <spencerzachariah@gmail.com>
2019-07-20 00:53:40 -0500

    improved the way the floating beam hazards work

--- scripts/items/MobileBeam.gd
+++ scripts/items/MobileBeam.gd
@@ -2,12 +2,14 @@ extends "res://scripts/items/Item.gd"
 
 var Laser = preload("res://scenes/map_elements/Laser.tscn")
 onready var fire_timer = Timer.new()
+onready var impulse_timer := $ImpulseTimer
 export var start_delay := 5
 export var frequency := 5
 export var duration := .25
 export var laser_delay := 1.5
 export var directions = [ Vector2.LEFT, Vector2.RIGHT, Vector2.UP, Vector2.DOWN]
 export var size := 30
+export var random_speed := 200
 
 func _ready():
 	add_child(fire_timer)
@@ -23,3 +25,23 @@ func _fire():
 		laser_add.delay = laser_delay
 		add_child(laser_add)
 		fire_timer.start(frequency)
+	impulse_timer.start()
+
+func _handle_random_motion():
+	var impulse_vector : Vector2
+	var torque_force := 250 * 10
+	var do_flip_x := randi() % 2
+	var do_flip_y := randi() % 2
+
+	impulse_vector.x = rand_range(random_speed, random_speed * 2)
+	impulse_vector.y = rand_range(random_speed, random_speed * 2)
+
+	if do_flip_x:
+		impulse_vector.x *= -1
+	if do_flip_y:
+		impulse_vector.y *= -1
+	apply_torque_impulse(torque_force)
+	apply_central_impulse(impulse_vector)
+
+func _on_ImpulseTimer_timeout():
+	_handle_random_motion()
