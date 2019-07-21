9ebd43b8b1b4afb5d787a411057f572767be23af
cslaf <cade.schlaefli@gmail.com>
2019-07-21 13:09:26 -0500

    changed round setter to use levers and signals

--- scripts/map_elements/levers/RoundSettingApparatus.gd
+++ scripts/map_elements/levers/RoundSettingApparatus.gd
@@ -3,3 +3,21 @@ extends Node2D
 export var num_rounds := 10
 export var min_rounds := 1
 export var max_rounds := 100
+
+onready var rounds_label = $RoundsLabel
+onready var infinite_label = $InfiniteLabel
+
+var infinite := false
+
+func increment(amount):
+	num_rounds = clamp(num_rounds + amount, min_rounds, max_rounds)
+	rounds_label.text = String(num_rounds)
+
+func toggle_infinite():
+	infinite = !infinite
+	Manager.repeats = infinite
+	rounds_label.visible = !infinite
+	if infinite :
+		infinite_label.text = "Infinite : On"
+	else :
+		infinite_label.text = "Infinite : Off"
