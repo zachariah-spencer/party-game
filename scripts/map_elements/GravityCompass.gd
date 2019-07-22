011af7fd9739ab31398a1cc04152380020fde559
cslaf <cade.schlaefli@gmail.com>
2019-07-21 23:10:14 -0500

    physics based implementation of gravityCompass

--- scripts/map_elements/GravityCompass.gd
+++ scripts/map_elements/GravityCompass.gd
@@ -1,15 +1,16 @@
 extends CanvasLayer
 
-onready var arrow := $Rig/Arrow
+onready var arrow := $Rig/weight/Arrow
 onready var tween := $Rig/Tween
 
 func _ready():
 	arrow.modulate = Color.red
-	arrow.rotation = -PI / 2
+#	arrow.rotation = -PI / 2
 
 func on_gravity_changed(new_gravity : Vector2):
 	var new_color
-	var new_rotation_dir = new_gravity.angle() + PI
+	$Rig/weight.apply_central_impulse(new_gravity.rotated(PI/2) * 100)
+#	var new_rotation_dir = new_gravity.angle() + PI
 	match new_gravity:
 		Vector2.UP:
 			new_color = Color.blue
@@ -24,5 +25,5 @@ func on_gravity_changed(new_gravity : Vector2):
 	tween.remove_all()
 	tween.stop_all()
 	tween.interpolate_property(arrow,'modulate',arrow.modulate, new_color,0.5,Tween.TRANS_LINEAR,Tween.EASE_IN_OUT)
-	tween.interpolate_property(arrow,'rotation',arrow.rotation,new_rotation_dir,.8,Tween.TRANS_SINE,Tween.EASE_IN_OUT)
+#	tween.interpolate_property(arrow,'rotation',arrow.rotation,new_rotation_dir,.8,Tween.TRANS_SINE,Tween.EASE_IN_OUT)
 	tween.start()
