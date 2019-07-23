bbde61d574f64c566b0132eb517a91c3402ace1c
cslaf <cade.schlaefli@gmail.com>
2019-07-23 15:20:06 -0500

    implemented variable camera speed in traps

--- scripts/minigame_scripts/mg_traps/CameraFollowPoint.gd
+++ scripts/minigame_scripts/mg_traps/CameraFollowPoint.gd
@@ -1,5 +1,17 @@
 extends Position2D
 
+var scroll_speed := .7
+
 func _physics_process(delta):
+	var far_players = 0
+	for body in $FasterScrollArea.get_overlapping_bodies() :
+		if body is Player : far_players += 1
+
+
 	if Manager.current_minigame.game_active && position.x < 3450:
-		position.x += 0.7
+		if far_players == 0 :
+			position.x += scroll_speed
+		elif far_players == 1 :
+			position.x +=  scroll_speed * 1.5
+		else :
+			position.x += scroll_speed * 2
