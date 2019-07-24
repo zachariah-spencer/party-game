eb0a8faecc27e808a00f5580a4e380663d0fad4b
zachariah-spencer <spencerzachariah@gmail.com>
2019-07-24 12:06:11 -0500

    work on procedural generation for paths game

--- scripts/minigame_scripts/mg_mirrored_paths/MG_MirroredPaths.gd
+++ scripts/minigame_scripts/mg_mirrored_paths/MG_MirroredPaths.gd
@@ -1,8 +1,13 @@
 extends Minigame
 
+const CHUNKS  = [
+	preload('res://scenes/minigames/mg_mirrored_paths/maps/any_mirroredpathsmap1/chunks/PathChunkOne.tscn'),
+]
+
+
 func _init():
 	maps = [
-	preload('res://scenes/minigames/mg_mirrored_paths/maps/Any_MirroredPathsMap1.tscn'),
+	preload('res://scenes/minigames/mg_mirrored_paths/maps/any_mirroredpathsmap1/Any_MirroredPathsMap1.tscn'),
 	]
 
 func _ready():
@@ -12,3 +17,20 @@ func _ready():
 func _run_minigame_loop():
 	if game_active:
 		_check_game_win_conditions()
+
+func update_chunk(old_chunk : Node2D):
+	#let all active chunks know that more are being loaded
+	emit_signal('updated_chunks')
+	CHUNKS.shuffle()
+	var new_chunk = CHUNKS[0].instance()
+	
+	while new_chunk.chunk_id == old_chunk.chunk_id:
+		CHUNKS.shuffle()
+		new_chunk = CHUNKS[0].instance()
+
+	#instance and set new chunk above players on the top of the tower
+	new_chunk.position = old_chunk.position
+	new_chunk.position.x -= old_chunk.size
+
+	#add to tree
+	call_deferred('add_child', new_chunk)
