acc6ee7c4d03da0115fb3da8b801846b96b4ea4a
cslaf <cade.schlaefli@gmail.com>
2019-07-24 19:43:45 -0500

    fixed color coding

--- scripts/map_elements/BeamHazard.gd
+++ scripts/map_elements/BeamHazard.gd
@@ -4,8 +4,10 @@ var Laser = preload("res://scenes/map_elements/Laser.tscn")
 export var beam_duration := .7
 export var beam_delay := 1.25
 
-func activate():
+func _ready():
 	$Base.modulate = outline_color
+
+func activate():
 	var laser = Laser.instance()
 	laser.duration = beam_duration
 	laser.delay = beam_delay
