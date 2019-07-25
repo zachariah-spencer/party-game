ad4a958670fe638e00c34dcb8cf4e579c57e192b
cslaf <cade.schlaefli@gmail.com>
2019-07-25 11:41:21 -0500

    removed dumb print statement

--- scripts/map_elements/CoundownCircle.gd
+++ scripts/map_elements/CoundownCircle.gd
@@ -21,7 +21,6 @@ func draw_circle_arc_poly(center, radius, angle_from, angle_to, color):
 	var points_arc = PoolVector2Array()
 	var colors = PoolColorArray([color])
 
-	print("active")
 	for i in range(nb_points + 1):
 		var angle_point = deg2rad(angle_from + i * (angle_to - angle_from) / nb_points - 90)
 		points_arc.push_back(center + Vector2(cos(angle_point), sin(angle_point)) * (radius * .4))
